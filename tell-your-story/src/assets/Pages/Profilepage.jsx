5c3e6c2c402f304ea08e6206906259fa09effdb9
Claire Zhu <claireliwen111@gmail.com>
2025-10-06 10:28:42 +0200

    testing

--- tell-your-story/src/assets/Pages/Profilepage.jsx
+++ tell-your-story/src/assets/Pages/Profilepage.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
 
+const { daysstate, setDaysstate } = useState();
 const Profilepage = () => {
-  return <div></div>;
+  useEffect(() => {
+    async function getAllDiary() {
+      try {
+        const { data } = await axios("http://localhost:5005/days");
+        console.log(data);
+        setDaysstate(data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getAllDiary();
+  }, []);
+  return (
+    <div>
+      <p>this is profile page</p>
+      {daysstate.map((oneday) => {
+        return (
+          <div>
+            <H3> oneday.date</H3>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Profilepage;
