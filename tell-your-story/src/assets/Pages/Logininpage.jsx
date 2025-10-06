b9c2bc61b8baae5d8f42d2c7bf575d44d5ba5215
Claire Zhu <claireliwen111@gmail.com>
2025-10-06 12:45:58 +0200

    profile page done

---
+++ tell-your-story/src/assets/Pages/Logininpage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Logininpage = () => {
+  return <div></div>;
+};
+
+export default Logininpage;
--- tell-your-story/src/assets/Pages/Profilepage.jsx
+++ tell-your-story/src/assets/Pages/Profilepage.jsx
@@ -1,24 +1,59 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const Profilepage = () => {
+<<<<<<< HEAD
   const [daysState, setDaysState] = useState([]);
   useEffect(() => {
     async function getAllDiary() {
       try {
         const { data } = await axios("http://localhost:5005/days");
         console.log(data);
+=======
+  const { userID } = useParams();
+
+  const [daysState, setDaysState] = useState([]);
+  const [userState, setuserState] = useState([]);
+
+  useEffect(() => {
+    async function getAllDiary() {
+      try {
+        const { data } = await axios(
+          `http://localhost:5005/days?userId=${userID}`
+        );
+
+>>>>>>> claireBranch
         setDaysState(data);
       } catch (err) {
         console.log(err);
       }
     }
     getAllDiary();
-  }, []);
+  }, [userID]);
+
+  useEffect(() => {
+    async function getUsername() {
+      try {
+        const { data } = await axios.get(
+          `http://localhost:5005/users?id=${userID}`
+        );
+        setuserState(data[0]);
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getUsername();
+  }, [userID]);
   return (
     <div>
+<<<<<<< HEAD
       <p>this is profile page</p>
+=======
+      <p>Good to see you, {userState.name}</p>
+>>>>>>> claireBranch
       {daysState.map((oneday) => {
         return (
           <div key={oneday.id}>
