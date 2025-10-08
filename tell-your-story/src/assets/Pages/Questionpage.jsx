0260df63e520f0c9440808bd5fe16ca7d6470b74
Vera Fileyeva <veremei.vera@gmail.com>
2025-10-08 13:22:43 +0100

    saving AI responce to DB added

--- tell-your-story/src/assets/Pages/Questionpage.jsx
+++ tell-your-story/src/assets/Pages/Questionpage.jsx
@@ -40,7 +40,7 @@ const Questionpage = () => {
   };
 
   //Change the format of the date to submit
-  function formateData() {
+  function formateData(AIresponce) {
     const notes = Object.keys(answers).map((oneAnswer) => {
       return {
         diary_promptsId: question[oneAnswer].id,
@@ -52,15 +52,15 @@ const Questionpage = () => {
       date: currentDate,
       userId: parseInt(userID),
       notes: notes,
+      AIparagraph: AIresponce,
     };
     return finalData;
   }
 
   // Function to submit
 
-  async function submitData(e) {
-    const dataToSubmit = formateData();
-    console.log("data to submit", dataToSubmit);
+  async function submitData(AIresponce) {
+    const dataToSubmit = formateData(AIresponce);
 
     try {
       const { data } = await axios.post(
@@ -82,8 +82,9 @@ const Questionpage = () => {
     alert("Well done for the day");
     console.log("All answers:", answers);
 
-    await handleSubmit();
-    await submitData();
+    const AIresponce = await handleSubmit(); //AI
+
+    await submitData(AIresponce); //save data to DB
   };
 
   function handleBacktoProfile() {
@@ -155,7 +156,6 @@ The user will give answers to these questions:
           },
         ],
       });
-      console.log("Full response:", response);
 
       const text = response.text;
       return text;
@@ -168,6 +168,7 @@ The user will give answers to these questions:
     const userPrompt = JSON.stringify(formateData());
     const reply = await getGPTResponse(userPrompt);
     setResponse(reply);
+    return reply;
   }
 
   //*****************end of Ai+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
@@ -194,13 +195,17 @@ The user will give answers to these questions:
           </div>
         )}
         {currentQuestionIndex === question.length - 1 ? (
-          <button onClick={handlelastQuestion}> Save </button>
+          <button className="prominent-btn" onClick={handlelastQuestion}>
+            Save
+          </button>
         ) : (
           <button className="prominent-btn" onClick={handleNextQuestion}>
             Next Question →
           </button>
         )}
-        <button onClick={handleBacktoProfile}> Back to profile </button>
+        <button className="not-so-prominent-btn" onClick={handleBacktoProfile}>
+          Back to profile
+        </button>
       </div>
     </div>
   );
